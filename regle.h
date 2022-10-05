3a9bd023f2863715f0ccf444e33bae8ff57ac43c
Samuel Gallay <samuel.gallay@gmail.com>
2022-10-05 23:24:38 +0200

    Implémentation du parser

--- regle.h
+++ regle.h
@@ -3,8 +3,8 @@
 
 typedef struct Regle_t Regle;
 
-Regle* Regle_create(char* nom );
+Regle *Regle_create(char *nom);
 
-void Regle_free(Regle * r);
+void Regle_free(Regle *r);
 
 #endif // REGLE_H_INCLUDED
